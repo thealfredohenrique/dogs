0671d883bed081cde3167d3d68a22d3fcd4af319
Alfredo Henrique <thealfredohenrique@gmail.com>
2021-12-29 23:24:45 -0300

    Add header component content

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/header.svg";
 import styles from "./styles.module.css";
 
 const Header = () => {
   return (
     <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Register</Link>
+      <nav className="container">
+        <Link to="/" className={styles.logo} aria-label="Home">
+          <Logo />
+        </Link>
+        <Link to="/login" className={styles.login}>
+          Login / Register
+        </Link>
       </nav>
     </header>
   );
