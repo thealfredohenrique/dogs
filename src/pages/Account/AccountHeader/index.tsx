6462bd0fcf8e1f1c33f506247ad83f4b2ae3c29f
Alfredo Henrique <thealfredohenrique@gmail.com>
2021-12-31 17:45:09 -0300

    Add navigation between routes on account page

--- src/pages/Account/AccountHeader/index.tsx
+++ src/pages/Account/AccountHeader/index.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import AccountNavigation from "../AccountNavigation";
 import styles from "./styles.module.css";
 
 const AccountHeader = () => {
+  const [title, setTitle] = useState("");
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    switch (pathname) {
+      case "/account/publish":
+        setTitle("Publish");
+        break;
+      case "/account/statistics":
+        setTitle("Statistics");
+        break;
+      default:
+        setTitle("My account");
+    }
+  }, [pathname]);
+
   return (
-    <header>
-      <h1 className="title">AccountHeader</h1>
+    <header className={styles.header}>
+      <h1 className="title">{title}</h1>
       <AccountNavigation />
     </header>
   );
--- src/pages/Account/AccountNavigation/index.tsx
+++ src/pages/Account/AccountNavigation/index.tsx
@@ -1,13 +1,30 @@
+import { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { UserContext } from "../../../contexts/UserContext";
+import { ReactComponent as FeedIcon } from "../../../assets/feed.svg";
+import { ReactComponent as AddIcon } from "../../../assets/add.svg";
+import { ReactComponent as StatisticsIcon } from "../../../assets/statistics.svg";
+import { ReactComponent as LogoutIcon } from "../../../assets/logout.svg";
 import styles from "./styles.module.css";
 
 const AccountNavigation = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  const { handleLogout } = useContext(UserContext);
+
   return (
-    <nav>
-      <NavLink to="/account">My photos</NavLink>
-      <NavLink to="/account/publish">Publish</NavLink>
-      <NavLink to="/account/statistics">Statistics</NavLink>
-      <button>Logout</button>
+    <nav className={styles.nav}>
+      <NavLink to="/account" end>
+        <FeedIcon /> {isMobile && "My photos"}
+      </NavLink>
+      <NavLink to="/account/publish">
+        <AddIcon /> {isMobile && "Publish"}
+      </NavLink>
+      <NavLink to="/account/statistics">
+        <StatisticsIcon /> {isMobile && "Statistics"}
+      </NavLink>
+      <button onClick={handleLogout}>
+        <LogoutIcon /> {isMobile && "Logout"}
+      </button>
     </nav>
   );
 };
