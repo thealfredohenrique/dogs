f4d3909535ec146cc073b5e331cbdbc2b39b58ae
Alfredo Henrique <alfredo.ribeiro@linx.com.br>
2022-05-28 20:55:58 -0300

    Deploy project to GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import NotFound from "./pages/NotFound";
 const App = () => {
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <UserStorage>
           <Header />
           <main className="app-body">
